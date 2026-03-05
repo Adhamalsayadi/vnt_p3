3162222b2d297f36da235945e19e699c9c2ca777
Adham Alsayadi <Adhamsalehalsayadi2@gmail.com>
2026-03-05 11:27:08 +0300

    edit

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,14 +1,21 @@
-// import { MetadataRoute } from "next";
+import { MetadataRoute } from "next";
 
-// const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
 
-// export default function sitemap(): MetadataRoute.Sitemap {
-//   const routes = ["/", "/about", "/contact", "/services", "/enquiries", "/advertisments"];
+export default function sitemap(): MetadataRoute.Sitemap {
+  const routes = [
+    "/",
+    "/about",
+    "/contact",
+    "/services",
+    "/enquiries",
+    "/advertisments",
+  ];
 
-//   return routes.map((route) => ({
-//     url: new URL(route, baseUrl).toString(),
-//     lastModified: new Date(),
-//     changeFrequency: route === "/" ? "daily" : "weekly",
-//     priority: route === "/" ? 1 : 0.8,
-//   }));
-// }
+  return routes.map((route) => ({
+    url: new URL(route, baseUrl).toString(),
+    lastModified: new Date(),
+    changeFrequency: route === "/" ? "daily" : "weekly",
+    priority: route === "/" ? 1 : 0.8,
+  }));
+}
